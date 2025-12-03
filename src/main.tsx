28a9f662ebe367c25998792833a2da04e2f2f889
Yevstratov Yevstratov <yevhen.y@thecomity.com>
2025-12-03 13:10:18 +0000

    Initialize project with React, TypeScript, and Vite setup, including ESLint and Prettier configurations, basic app structure, and essential dependencies.

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material';
+
+import theme from './theme';
+
+createRoot(document.getElementById('root')!).render(
+  <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </>
+);
